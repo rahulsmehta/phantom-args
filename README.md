4f84d14efdf1e064bf369dfe8bee13770212c994
Rahul Mehta <mehta.rahul95@gmail.com>
2012-08-18 14:28:14 -0700

    Added stub for double flag and updated docs

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 phantom-args
-============
+-----------
 
 Command line argument utility for PhantomJS
+### Features
+* Specify arguments in `args.json`
+* Get a nice json object back containing all the values that were set in the args
+* Get on with writing your program rather than processing the args!
+
+### Coming soon
+* Similar argument processor for Node.js applications
+* Possibly an all-in-one project
--- args.js
+++ args.js
@@ -3,17 +3,15 @@ var sys = require('system'),
 		scriptArgs = sys.args.slice(1),
 		argInfo = "";
 
-phantom.injectJs("lib/json2.js");
+phantom.injectJs("lib/json2.js"); //not sure if necessary, might remove
 
 //Load args information from args.json
 if(fs.exists("./args.json")){
 	console.log("Reading args.json...");
 	argInfo = JSON.parse(fs.read("./args.json"));
-	console.log(argInfo);
 }
 
 
-console.log(scriptArgs);
 
 //Handle uses cases/types for command line flags
 //-d -n, etc. stuff like that will be flagged if the first
@@ -26,10 +24,14 @@ function processArgs(args){
 	while(args.length > 0){
 		var _arg= args.pop();
 		
-		if(utils.isDouble(_arg)){} //process as double-flagged argument
+		if(utils.isDouble(_arg)){
+			console.log("We've got a double here!");
+		} //process as double-flagged argument
 
 		if(utils.isFlag(_arg)){ //processing a flagged argument
-			
+			if(typeof argInfo[_arg]	!== "undefined"){
+				console.log("Expected subsequent argument to be typeof "+argInfo[_arg]);
+			}
 		}
 	
 				
@@ -42,9 +44,12 @@ var utils = {
 		return (str.charAt(0) === '-' && str.charAt(1) === '-');
 	},
 	"isFlag":function(str){
-		return (str.charAt(0) === '-');
+		return (str.charAt(0) === '-' && str.length == 2);
 	}
 
 }
+
+
+processArgs(scriptArgs);
 
 phantom.exit();
