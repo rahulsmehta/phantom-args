e604266ff692345f9e74f62f912292ee86132a61
Rahul Mehta <mehta.rahul95@gmail.com>
2012-08-18 21:34:47 -0700

    Refactored args.json and added result object

--- args.js
+++ args.js
@@ -1,7 +1,6 @@
 var sys = require('system'),
 		fs = require('fs');
-		scriptArgs = sys.args.slice(1),
-		argInfo = "";
+		scriptArgs = sys.args.slice(1);
 
 phantom.injectJs("lib/json2.js"); //not sure if necessary, might remove
 phantom.injectJs("lib/jquery.min.js");
@@ -9,7 +8,7 @@ phantom.injectJs("lib/jquery.min.js");
 //Load args information from args.json
 if(fs.exists("./args.json")){
 	console.log("Reading args.json...");
-	argInfo = JSON.parse(fs.read("./args.json"));
+	arg_list = JSON.parse(fs.read("./args.json"));
 }
 
 
@@ -22,19 +21,37 @@ if(fs.exists("./args.json")){
 
 
 function processArgs(args){
-	while(args.length > 0){
-		var _arg= args.pop();
-		
+	var result = {};
+	for (var key in arg_list){
+		result[key] = {
+			"required":arg_list[key].required,
+			"found":false,
+			"value":null
+		};
+	}
+	for(var i=0; i<args.length; ++i){
+		var _arg = args[i];
 		if(utils.isFlag(_arg)){ //processing a flagged argument
-			for(var key in argInfo){
-				if($.inArray(_arg,key.flag)){
-					console.log("We found "+_arg);
+		
+			for(var key in arg_list){
+				var _flag = arg_list[key].flag
+				
+				if(utils.arrContains(_flag,_arg)){
+					var log_str = key+" found"; 
+					result[key].found=true;
+					if(arg_list[key].type === "param"){
+						var val = args[i+1];
+						log_str+=": val -> "+val;
+						result[key].value = val;
+					}
+					console.log(log_str);
+					break;
 				}
 			}
 		}
-	
 				
 	}
+	console.log(JSON.stringify(result));
 
 }
 
@@ -42,10 +59,12 @@ var utils = {
 	"isFlag":function(str){
 		var flag = (str.charAt(0) === '-' && str.charAt(1) === '-') || (str.charAt(0) === '-' && str.length == 2);
 		return flag;
+	},
+	"arrContains":function(arr,val){
+		return arr.indexOf(val) != -1;
 	}
 
 }
-
 
 processArgs(scriptArgs);
 
